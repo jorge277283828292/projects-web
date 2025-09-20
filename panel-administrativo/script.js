972a3f03d9a46ab6ea07b1cf33e19332128c46ca
Jorge Villarreal <jv7484031@gmail.com>
2025-09-20 12:52:54 -0600

    Programa final.

--- panel-administrativo/script.js
+++ panel-administrativo/script.js
@@ -1,7 +1,35 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
-const themeToggler = document.querySelector(".themeToggler");
+const themeToggler = document.querySelector(".theme-toggler");
+
+// Datos de órdenes
+const Orders = [
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'LARVENDER KF102 Drone',
+    productNumber: '36378',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'Ruko F11 Pro Drone',
+    productNumber: '49347',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Drone with Camera Drone',
+    productNumber: '96996',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  }
+];
 
 menuBtn.addEventListener('click', () => {
   sideMenu.style.display = 'block';
